0f34889debb8b9f57912ee442edee22f9809f38e
CaptCaptain <badtimeeric@gmail.com>
2021-01-26 17:19:07 -0500

    Updated
    * Fixed strings and seperated card data
    * Added more maps

--- src/shared/mapData.js
+++ src/shared/mapData.js
@@ -7,13 +7,27 @@ let availableMapData = {
     },
     WORKSHOP_ISLAND: {
         CENTER: '[vect(0, 0, 0), 7]'
+    },
+    ILIOS_RUINS: {
+        CENTER: '[vect(28.414, 57.353, -157.183), 6]'
+    },
+    VOLSKAYA: {
+        CENTER: '[vect(-13.680, -3.713, 90.259), 7]'
+    },
+    BLIZZ_WORLD: {
+        CENTER: '[vect(-122.206, -0.156, 110.472), 7]'
+    },
+    DORADO: {
+        CENTER: '[vect(131.219, 11.030, 26.117), 7]'
+    },
+    LIJIANG_NIGHT_MARKET: {
+        CENTER: '[vect(1.193, -0.996, -60.635), 7]'
     }
 }
 
 // Some maps share the same map data
 availableMapData.WORKSHOP_ISLAND_NIGHT = availableMapData.WORKSHOP_ISLAND
-availableMapData.WORKSHOP_CHAMBER = availableMapData.WORKSHOP_ISLAND
-availableMapData.WORKSHOP_GREEN_SCREEN = availableMapData.WORKSHOP_ISLAND
+availableMapData.LIJIANG_NIGHT_MARKET_LNY = availableMapData.LIJIANG_NIGHT_MARKET
 
 let maps = ''
 let center = ''
