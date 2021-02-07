9cc8684f78f8dca49644a05cd8dfbf441e85effd
CaptCaptain <badtimeeric@gmail.com>
2021-02-06 20:18:10 -0500

    Update
    * Updated control scheme to be more user friendly
    * Added a fail-safe in cases where the game timer stops.

--- src/ata/string.js
+++ src/ata/string.js
@@ -40,14 +40,14 @@ const translations = {
     },
     JUDGE_VOTE_INSTRUCTION: {
         "en-US": "Card #{}/{}\n{} - Confirm\n{} (Left) / {} (Right) - Select",
-        format: "cardJudge.selectedCardJudge, len(getJudgeCards) - 1, buttonString(Button.PRIMARY_FIRE), buttonString(Button.ULTIMATE), buttonString(Button.ABILITY_2)"
+        format: "cardJudge.selectedCardJudge, len(getJudgeCards) - 1, buttonString(Button.PRIMARY_FIRE), buttonString(Button.ABILITY_1), buttonString(Button.ABILITY_2)"
     },
     YOUR_CARDS: {
         "en-US": "Your Cards"
     },
     CARD_CONTROLS: {
         "en-US": " \n{} - Play Card\n{} - Get New Cards (Forfeit your turn)\n{} (Up) / {} (Down) - Select Card",
-        format: "buttonString(Button.PRIMARY_FIRE), buttonString(Button.SECONDARY_FIRE), buttonString(Button.RELOAD), buttonString(Button.INTERACT)"
+        format: "buttonString(Button.PRIMARY_FIRE), buttonString(Button.SECONDARY_FIRE), buttonString(Button.ABILITY_2), buttonString(Button.ABILITY_1)"
     },
     JUDGE_VOTED:{
         "en-US": "The Judge has voted for {}",
